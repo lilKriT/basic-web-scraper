321e0c2f9dccc93236b76c85f1e4d1cb24ddcb1a
Piotr Filipiak <kiddhkane@gmail.com>
2022-12-13 18:06:12 +0100

    Created a basic server.

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const PORT = 8000;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
